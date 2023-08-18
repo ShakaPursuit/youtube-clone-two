f991def7c100f35733d050346ff7cd1743852824
Christine Marchese <chm978@gmail.com>
2023-08-18 10:48:04 -0400

    first change

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+    
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React App1</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
