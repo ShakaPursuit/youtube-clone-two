ea74d1f720ff8ed3f443c44cc8234ff912f73cfd
ShakaPursuit <shakalivngstone@pursuit.org>
2023-08-18 12:12:55 -0400

    added home componenet

---
+++ src/HomePage.jsx
@@ -0,0 +1,44 @@
+ function Home(){
+
+
+
+
+
+    return (<>
+   <div>
+    <header className="header">
+
+
+        <a>Youtube</a><br></br><br></br>&nbsp; 
+        <a>Home</a><br></br>&nbsp; 
+        <a>About</a>
+
+
+
+    </header>
+    </div>
+
+   <>
+   <input type="text" ></input><button>Search</button>
+   
+   
+   </>
+
+
+{/*  */}
+
+  
+
+    <div className="videos">No Search Results Yet! Please Submit a search above!</div>
+    
+    
+    
+    
+    
+    </>);
+ 
+
+
+}
+
+export default Home
