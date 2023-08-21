42803952f7a6d6a30b9c67cbae9eb95f9281a4eb
ShakaPursuit <shakalivngstone@pursuit.org>
2023-08-21 12:27:56 -0400

    deleted useless code

--- src/App.jsx
+++ src/App.jsx
@@ -20,33 +20,10 @@ function App() {
       <div>
       <div className="top"><Home/></div> 
         {/* <NavBar/> */}
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
+        
       </div>
      
-      {/* <h1>Vite + React!!!!!!!!!!!</h1>
- 
-      <h1>Christine</h1>
-      <h1>Vite + React + Jesse</h1>
-
-      <h1>Vite + React + Jesse Attempt 2</h1>
-
-
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more!!!
-      </p> */}
+      
     </>
 
   )
