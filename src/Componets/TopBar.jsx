2f101eaaeb65757e790b62391ea51900a88e7c5c
ShakaPursuit <shakalivngstone@pursuit.org>
2023-09-05 00:41:12 -0400

    changed logo

--- src/Componets/TopBar.jsx
+++ src/Componets/TopBar.jsx
@@ -3,7 +3,7 @@ export default function TopBar() {
   return (
     <header className="header">
       <li>
-        <img className="icon" src="src/assets/yticon.png"></img>
+        <img className="icon" src="https://w7.pngwing.com/pngs/24/227/png-transparent-youtube-logo-youtube-text-trademark-logo.png"></img>
       </li>
       <br></br>
       <br></br>&nbsp;
